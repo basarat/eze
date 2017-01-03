550013260ed592d100db8f7fe2f36bf5499790e4
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-03 18:07:18 +1100

    better

--- src/internal/collector.ts
+++ src/internal/collector.ts
@@ -1,12 +1,33 @@
 import { Config, Data } from '../types';
 import { toHtml, dedent } from './markdown';
+import { bundle } from './bundler';
+import * as fse from 'fs-extra';
+
+const appIndexTemplate = ({ index, jsFileName }: { index: number, jsFileName: string }) => `
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <title>Demo: ${index}</title>
+</head>
+
+<body>
+  <div id="root"></div>
+  <script type="text/javascript" src="./${jsFileName}"></script>
+</body>
+</html>
+`;
 
 /**
  * Collects a document
  */
 export class Collector {
 
-  constructor(private config: Config) {}
+  constructor(private config: Config) { }
 
   /**
    * We collect the rendered contents here
@@ -15,9 +36,32 @@ export class Collector {
     contents: []
   }
 
+  /** Each demo gets its index */
+  private entryPointIndex = 0;
+
   async md(markdown: string) {
     /** render the markdown */
     this.data.contents.push({ type: 'html', html: toHtml(dedent(markdown)) });
     /** TODO: Collect headings in table of contents */
+  }
+
+  async app(entryPointPath: string) {
+    this.entryPointIndex++;
+
+    const index = this.entryPointIndex;
+    const jsFileName = `app-${this.entryPointIndex}.js`;
+    const htmlFileName = `app-${this.entryPointIndex}.html`;
+
+    /** TODO: collect in data */
+    const outputFileName = `${this.config.outputDir}/${jsFileName}`;
+
+    /** Write the html */
+    fse.outputFileSync(
+      this.config.outputDir + `/${htmlFileName}`,
+      appIndexTemplate({ index, jsFileName })
+    );
+
+    /** Bundle */
+    await bundle({ entryPointName: entryPointPath, outputFileName: outputFileName });
   }
 }
