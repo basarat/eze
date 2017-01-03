051a53b17112855c3625bbd4ad807239f369e70a
basarat <basarat@example.com>
2017-01-03 06:42:56 +0000

    [ci skip] deployment (2017-1-3-6-42)

--- app.js
+++ app.js
@@ -22930,6 +22930,60 @@
 	        .trim());
 	}
 	exports.toHtml = toHtml;
+	/**
+	 * Dedent template strings. Great for markdown authoring
+	 * https://github.com/dmnd/dedent/blob/master/dedent.js
+	 */
+	function dedent(strings) {
+	    var values = [];
+	    for (var _i = 1; _i < arguments.length; _i++) {
+	        values[_i - 1] = arguments[_i];
+	    }
+	    var raw;
+	    if (typeof strings === "string") {
+	        // dedent can be used as a plain function
+	        raw = [strings];
+	    }
+	    else {
+	        raw = strings.raw;
+	    }
+	    // first, perform interpolation
+	    var result = "";
+	    for (var i = 0; i < raw.length; i++) {
+	        result += raw[i].
+	            // join lines when there is a suppressed newline
+	            replace(/\\\n[ \t]*/g, "").
+	            // handle escaped backticks
+	            replace(/\\`/g, "`");
+	        if (i < values.length) {
+	            result += values[i];
+	        }
+	    }
+	    // dedent eats leading and trailing whitespace too
+	    result = result.trim();
+	    // now strip indentation
+	    var lines = result.split("\n");
+	    var mindent = null;
+	    lines.forEach(function (l) {
+	        var m = l.match(/^ +/);
+	        if (m) {
+	            var indent = m[0].length;
+	            if (!mindent) {
+	                // this is the first indented line
+	                mindent = indent;
+	            }
+	            else {
+	                mindent = Math.min(mindent, indent);
+	            }
+	        }
+	    });
+	    if (mindent !== null) {
+	        result = lines.map(function (l) { return l[0] === " " ? l.slice(mindent) : l; }).join("\n");
+	    }
+	    // handle escaped newlines at the end to ensure they don't get stripped too
+	    return result.replace(/\\n/g, "\n");
+	}
+	exports.dedent = dedent;
 
 
 /***/ },
