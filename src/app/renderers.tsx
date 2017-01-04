7f112bf389dc36f426de7303ad90f32eca1eac0d
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-04 16:53:54 +1100

    move stuff out

--- src/app/renderers.tsx
+++ src/app/renderers.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import * as types from '../types';
-import { style, classes } from 'typestyle';
+import { style, classes, cssRaw } from 'typestyle';
 import * as csstips from 'csstips';
 import * as styles from '../internal/styles';
 import { highlightCodeWithMode, MarkDownStyles } from '../internal/markdown';
-import { Collapsible } from './collapsible';
+import { Expandible } from 'expandible';
+import Toggle from 'react-toggle'
 
 export class HtmlRenderer extends React.PureComponent<types.HTMLContent, {}> {
   render() {
@@ -24,10 +25,12 @@ namespace AppRendererStyles {
   }
 
   export const iframe = style({
-    borderTop: `10px solid ${borderColor}`,
-    borderBottom: `10px solid ${borderColor}`,
+    display: 'block',
+
+    borderTop: `1px solid ${borderColor}`,
+    borderBottom: `1px solid ${borderColor}`,
     borderLeft: `2px solid ${borderColor}`,
-    borderRight: `2px solid ${borderColor}`,
+    borderRight: `1px solid ${borderColor}`,
 
     transition: 'width .2s',
   });
@@ -72,17 +75,34 @@ export class AppRenderer extends React.PureComponent<types.AppContent, { mode?: 
 
 
       {/** View code toggle */}
-      <button onClick={(open) => this.setState({ viewCode: !this.state.viewCode })}>Toggle Code</button>
+      <div>
+        <label style={{ display: 'inline-block' }}>
+          <Toggle
+            value={this.state.viewCode}
+            onChange={() => this.setState({ viewCode: !this.state.viewCode })} />
+          <span style={{
+            color: styles.colors.text,
+            fontSize: '16px',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+            paddingLeft: '5px',
+            paddingRight: '5px',
+            position: 'relative',
+            top: '-5px',
+            userSelect: 'none',
+          }}>Show Code</span>
+        </label>
+      </div>
 
       {/** Code */}
-      <Collapsible open={this.state.viewCode}>
+      <Expandible open={this.state.viewCode}>
         {/** Render code in same dom structure as markdown would. To reuse styles */}
         <div className={MarkDownStyles.rootClass}>
           <pre className={style({ margin: '0px' })}>
             <code dangerouslySetInnerHTML={{ __html: highlightCodeWithMode(props.sources[0]) }} />
           </pre>
         </div>
-      </Collapsible>
+      </Expandible>
 
     </div>;
   }
@@ -136,3 +156,146 @@ class Breakpoints extends React.PureComponent<{ mode: AppMode, onModeChange: (mo
     </div>;
   }
 }
+
+/** https://raw.githubusercontent.com/instructure-react/react-toggle/master/style.css */
+cssRaw(`
+.react-toggle {
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  background-color: transparent;
+  border: 0;
+  padding: 0;
+
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  -webkit-tap-highlight-color: transparent;
+}
+
+.react-toggle-screenreader-only {
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+}
+
+.react-toggle--disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
+  -webkit-transition: opacity 0.25s;
+  transition: opacity 0.25s;
+}
+
+.react-toggle-track {
+  width: 50px;
+  height: 24px;
+  padding: 0;
+  border-radius: 30px;
+  background-color: #4D4D4D;
+  -webkit-transition: all 0.2s ease;
+  -moz-transition: all 0.2s ease;
+  transition: all 0.2s ease;
+}
+
+.react-toggle:hover:not(.react-toggle--disabled) .react-toggle-track {
+  background-color: #000000;
+}
+
+.react-toggle--checked .react-toggle-track {
+  background-color: #19AB27;
+}
+
+.react-toggle--checked:hover:not(.react-toggle--disabled) .react-toggle-track {
+  background-color: #128D15;
+}
+
+.react-toggle-track-check {
+  position: absolute;
+  width: 14px;
+  height: 10px;
+  top: 0px;
+  bottom: 0px;
+  margin-top: auto;
+  margin-bottom: auto;
+  line-height: 0;
+  left: 8px;
+  opacity: 0;
+  -webkit-transition: opacity 0.25s ease;
+  -moz-transition: opacity 0.25s ease;
+  transition: opacity 0.25s ease;
+}
+
+.react-toggle--checked .react-toggle-track-check {
+  opacity: 1;
+  -webkit-transition: opacity 0.25s ease;
+  -moz-transition: opacity 0.25s ease;
+  transition: opacity 0.25s ease;
+}
+
+.react-toggle-track-x {
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  top: 0px;
+  bottom: 0px;
+  margin-top: auto;
+  margin-bottom: auto;
+  line-height: 0;
+  right: 10px;
+  opacity: 1;
+  -webkit-transition: opacity 0.25s ease;
+  -moz-transition: opacity 0.25s ease;
+  transition: opacity 0.25s ease;
+}
+
+.react-toggle--checked .react-toggle-track-x {
+  opacity: 0;
+}
+
+.react-toggle-thumb {
+  transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+  position: absolute;
+  top: 1px;
+  left: 1px;
+  width: 22px;
+  height: 22px;
+  border: 1px solid #4D4D4D;
+  border-radius: 50%;
+  background-color: #FAFAFA;
+
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+
+  -webkit-transition: all 0.25s ease;
+  -moz-transition: all 0.25s ease;
+  transition: all 0.25s ease;
+}
+
+.react-toggle--checked .react-toggle-thumb {
+  left: 27px;
+  border-color: #19AB27;
+}
+
+.react-toggle--focus .react-toggle-thumb {
+  -webkit-box-shadow: 0px 0px 3px 2px #0099E0;
+  -moz-box-shadow: 0px 0px 3px 2px #0099E0;
+  box-shadow: 0px 0px 2px 3px #0099E0;
+}
+
+.react-toggle:active:not(.react-toggle--disabled) .react-toggle-thumb {
+  -webkit-box-shadow: 0px 0px 5px 5px #0099E0;
+  -moz-box-shadow: 0px 0px 5px 5px #0099E0;
+  box-shadow: 0px 0px 5px 5px #0099E0;
+}
+`);
