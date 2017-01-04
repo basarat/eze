9130de428f29d974ada62cfca823d3498c24e3aa
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-04 11:29:43 +1100

    iframe styling

--- src/app/renderers.tsx
+++ src/app/renderers.tsx
@@ -1,17 +1,49 @@
 import * as React from 'react';
 import * as types from '../types';
-
+import { style, classes } from 'typestyle';
 
 export class HtmlRenderer extends React.PureComponent<types.HTMLContent, {}> {
   render() {
-    const {props} = this;
+    const { props } = this;
     return <div dangerouslySetInnerHTML={{ __html: props.html }} />;
   }
 }
 
-export class AppRenderer extends React.PureComponent<types.AppContent, {}> {
+namespace AppRendererStyles {
+  const borderColor = '#bbb'
+  export const iframe = style({
+    borderTop: `10px solid ${borderColor}`,
+    borderBottom: `10px solid ${borderColor}`,
+    borderLeft: `2px solid ${borderColor}`,
+    borderRight: `2px solid ${borderColor}`,
+  });
+
+  export const auto = style({
+    width: '100%',
+  });
+
+  export const desktop = style({
+    width: '100%',
+  });
+
+  export const tablet = style({
+    width: '100%',
+  });
+
+  export const mobile = style({
+    width: '100%',
+  });
+}
+
+export class AppRenderer extends React.PureComponent<types.AppContent, { mode: 'auto' | 'desktop' | 'tablet' | 'mobile' }> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'auto'
+    }
+  }
   render() {
-    const {props} = this;
-    return <iframe src={`./${props.htmlFileName}`}/>;
+    const { props } = this;
+    return <iframe className={classes(AppRendererStyles.iframe, AppRendererStyles[this.state.mode])} src={`./${props.htmlFileName}`} />;
   }
 }
