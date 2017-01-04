5a557a6a6b579f682c7d8f0ddb7fbed5e8ae752c
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-04 15:07:24 +1100

    bring in collapsible to TS

--- src/app/renderers.tsx
+++ src/app/renderers.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as types from '../types';
 import { style, classes } from 'typestyle';
 import * as styles from '../internal/styles';
-import {highlightCodeWithMode, MarkDownStyles} from '../internal/markdown';
+import { highlightCodeWithMode, MarkDownStyles } from '../internal/markdown';
 
 export class HtmlRenderer extends React.PureComponent<types.HTMLContent, {}> {
   render() {
@@ -99,7 +99,7 @@ class Breakpoints extends React.PureComponent<{ mode: AppMode, onModeChange: (mo
         '&>*:first-child': {
           borderLeft: '2px solid #ccc !important'
         },
-        '&>*:last-child':{
+        '&>*:last-child': {
           borderRight: '2px solid #ccc !important'
         },
       }
@@ -123,217 +123,210 @@ class Breakpoints extends React.PureComponent<{ mode: AppMode, onModeChange: (mo
   }
 }
 
+type CollapsibleProps = {
+  transitionTime: number,
+  easing: string,
+  open: boolean,
+  classParentString: string,
+  handleTriggerClick: (accordionPosition: number) => void,
+  trigger: string | HTMLElement,
+  triggerWhenOpen: string | HTMLElement,
+  lazyRender: boolean,
+  overflowWhenOpen: 'hidden' | 'visible' | 'auto' | 'scroll' | 'inherit' | 'initial' | 'unset'
 
-// export class Collapsible extends React.PureComponent <{}, {}>{
+  /** To work in an accordian */
+  accordionPosition?: number,
+}
+/** Based on https://www.npmjs.com/package/react-collapsible */
+export class Collapsible extends React.PureComponent<CollapsibleProps, {
+  isClosed?: boolean,
+  shouldSwitchAutoOnNextCycle?: boolean,
+  height?: string | number,
+  transition?: string,
+  hasBeenOpened?: boolean,
+  overflow?: CollapsibleProps['overflowWhenOpen'],
+}>{
 
-//   //Set validation for prop types
-//   propTypes: {
-//     transitionTime: React.PropTypes.number,
-//     easing: React.PropTypes.string,
-//     open: React.PropTypes.bool,
-//     classParentString: React.PropTypes.string,
-//     accordionPosition: React.PropTypes.number,
-//     handleTriggerClick: React.PropTypes.func,
-//     trigger: React.PropTypes.oneOfType([
-//       React.PropTypes.string,
-//       React.PropTypes.element
-//     ]),
-//     triggerWhenOpen:React.PropTypes.oneOfType([
-//       React.PropTypes.string,
-//       React.PropTypes.element
-//     ]),
-//     lazyRender: React.PropTypes.bool,
-//     overflowWhenOpen: React.PropTypes.oneOf([
-//       'hidden',
-//       'visible',
-//       'auto',
-//       'scroll',
-//       'inherit',
-//       'initial',
-//       'unset'
-//     ])
-//   },
+  //If no transition time or easing is passed then default to this
+  static defaultProps = {
+    transitionTime: 400,
+    easing: 'linear',
+    open: false,
+    classParentString: 'Collapsible',
+    lazyRender: false,
+    overflowWhenOpen: 'hidden',
 
-//   //If no transition time or easing is passed then default to this
-//   static defaultProps = {
-//     transitionTime: 400,
-//     easing: 'linear',
-//     open: false,
-//     classParentString: 'Collapsible',
-//     lazyRender: false,
-//     overflowWhenOpen: 'hidden'
-//   };
+    accordionPosition: 0
+  };
 
-//   //Defaults the dropdown to be closed
-//   getInitialState: function(){
+  constructor(props: CollapsibleProps) {
+    super(props);
 
-//     if(this.props.open){
-//       return {
-//         isClosed: false,
-//         shouldSwitchAutoOnNextCycle: false,
-//         height: 'auto',
-//         transition: 'none',
-//         hasBeenOpened: true,
-//         overflow: this.props.overflowWhenOpen
-//       }
-//     }
-//     else{
-//       return {
-//         isClosed: true,
-//         shouldSwitchAutoOnNextCycle: false,
-//         height: 0,
-//         transition: 'height ' + this.props.transitionTime + 'ms ' + this.props.easing,
-//         hasBeenOpened: false,
-//         overflow: 'hidden'
-//       }
-//     }
-//   },
+    this.state = this.props.open ? {
+      isClosed: false,
+      shouldSwitchAutoOnNextCycle: false,
+      height: 'auto',
+      transition: 'none',
+      hasBeenOpened: true,
+      overflow: this.props.overflowWhenOpen
+    }
+      : {
+        isClosed: true,
+        shouldSwitchAutoOnNextCycle: false,
+        height: 0,
+        transition: 'height ' + this.props.transitionTime + 'ms ' + this.props.easing,
+        hasBeenOpened: false,
+        overflow: 'hidden'
+      }
+  };
 
-//   // Taken from https://github.com/EvandroLG/transitionEnd/
-//   // Determines which prefixed event to listen for
-//   whichTransitionEnd: function(element){
-//       var transitions = {
-//           'WebkitTransition' : 'webkitTransitionEnd',
-//           'MozTransition'    : 'transitionend',
-//           'OTransition'      : 'oTransitionEnd otransitionend',
-//           'transition'       : 'transitionend'
-//       };
+  // Taken from https://github.com/EvandroLG/transitionEnd/
+  // Determines which prefixed event to listen for
+  whichTransitionEnd = (element) => {
+    var transitions = {
+      'WebkitTransition': 'webkitTransitionEnd',
+      'MozTransition': 'transitionend',
+      'OTransition': 'oTransitionEnd otransitionend',
+      'transition': 'transitionend'
+    };
 
-//       for(var t in transitions){
-//           if(element.style[t] !== undefined){
-//               return transitions[t];
-//           }
-//       }
-//   },
+    for (var t in transitions) {
+      if (element.style[t] !== undefined) {
+        return transitions[t];
+      }
+    }
+  }
 
-//   componentDidMount: function() {
-//     //Set up event listener to listen to transitionend so we can switch the height from fixed pixel to auto for much responsiveness;
-//     //TODO:  Once Synthetic transitionend events have been exposed in the next release of React move this funciton to a function handed to the onTransitionEnd prop
+  refs: {
+    outer?: HTMLDivElement
+    inner?: HTMLDivElement
+  };
 
-//     this.refs.outer.addEventListener(this.whichTransitionEnd(this.refs.outer), (event) => {
-//       if(this.state.isClosed === false){
-//         this.setState({
-//           shouldSwitchAutoOnNextCycle: true
-//         });
-//       }
+  componentDidMount = () => {
+    // Set up event listener to listen to transitionend so we can switch the height from fixed pixel to auto for much responsiveness;
+    this.refs.outer.addEventListener(this.whichTransitionEnd(this.refs.outer), (event) => {
+      if (this.state.isClosed === false) {
+        this.setState({
+          shouldSwitchAutoOnNextCycle: true
+        });
+      }
 
-//     });
-//   },
+    });
+  }
 
-//   componentDidUpdate: function(prevProps) {
+  componentDidUpdate = (prevProps:CollapsibleProps) => {
 
-//     if(this.state.shouldSwitchAutoOnNextCycle === true && this.state.isClosed === false) {
-//       //Set the height to auto to make compoenent re-render with the height set to auto.
-//       //This way the dropdown will be responsive and also change height if there is another dropdown within it.
-//       this.makeResponsive();
-//     }
+    if (this.state.shouldSwitchAutoOnNextCycle === true && this.state.isClosed === false) {
+      //Set the height to auto to make compoenent re-render with the height set to auto.
+      //This way the dropdown will be responsive and also change height if there is another dropdown within it.
+      this.makeResponsive();
+    }
 
-//     if(this.state.shouldSwitchAutoOnNextCycle === true && this.state.isClosed === true) {
-//       this.prepareToOpen();
-//     }
+    if (this.state.shouldSwitchAutoOnNextCycle === true && this.state.isClosed === true) {
+      this.prepareToOpen();
+    }
 
-//     //If there has been a change in the open prop (controlled by accordion)
-//     if(prevProps.open != this.props.open) {
-//       console.log('Open state changed!', this.props.accordionPosition);
+    //If there has been a change in the open prop (controlled by accordion)
+    if (prevProps.open != this.props.open) {
+      console.log('Open state changed!', this.props.accordionPosition);
 
-//       if(this.props.open === true) {
-//         this.openCollapsible();
-//       }
-//       else {
-//         this.closeCollapsible();
-//       }
-//     }
-//   },
+      if (this.props.open === true) {
+        this.openCollapsible();
+      }
+      else {
+        this.closeCollapsible();
+      }
+    }
+  }
 
 
-//   handleTriggerClick: function(event) {
+  handleTriggerClick = (event) => {
 
-//     event.preventDefault();
+    event.preventDefault();
 
-//     if(this.props.handleTriggerClick) {
-//       this.props.handleTriggerClick(this.props.accordionPosition);
-//     }
-//     else{
+    if (this.props.handleTriggerClick) {
+      this.props.handleTriggerClick(this.props.accordionPosition);
+    }
+    else {
 
-//       if(this.state.isClosed === true){
-//         this.openCollapsible();
-//       }
-//       else {
-//         this.closeCollapsible();
-//       }
-//     }
+      if (this.state.isClosed === true) {
+        this.openCollapsible();
+      }
+      else {
+        this.closeCollapsible();
+      }
+    }
 
-//   },
+  }
 
-//   closeCollapsible: function() {
-//     this.setState({
-//       isClosed: true,
-//       shouldSwitchAutoOnNextCycle: true,
-//       height: this.refs.inner.offsetHeight,
-//       overflow: 'hidden',
-//     });
-//   },
+  closeCollapsible = () => {
+    this.setState({
+      isClosed: true,
+      shouldSwitchAutoOnNextCycle: true,
+      height: this.refs.inner.offsetHeight,
+      overflow: 'hidden',
+    });
+  }
 
-//   openCollapsible: function() {
-//     this.setState({
-//       height: this.refs.inner.offsetHeight,
-//       transition: 'height ' + this.props.transitionTime + 'ms ' + this.props.easing,
-//       isClosed: false,
-//       hasBeenOpened: true
-//     });
-//   },
+  openCollapsible = () => {
+    this.setState({
+      height: this.refs.inner.offsetHeight,
+      transition: 'height ' + this.props.transitionTime + 'ms ' + this.props.easing,
+      isClosed: false,
+      hasBeenOpened: true
+    });
+  }
 
-//   makeResponsive: function() {
-//     this.setState({
-//       height: 'auto',
-//       transition: 'none',
-//       shouldSwitchAutoOnNextCycle: false,
-//       overflow: this.props.overflowWhenOpen
-//     });
-//   },
+  makeResponsive = () => {
+    this.setState({
+      height: 'auto',
+      transition: 'none',
+      shouldSwitchAutoOnNextCycle: false,
+      overflow: this.props.overflowWhenOpen
+    });
+  }
 
-//   prepareToOpen: function() {
-//     //The height has been changes back to fixed pixel, we set a small timeout to force the CSS transition back to 0 on the next tick.
-//     window.setTimeout(() => {
-//       this.setState({
-//         height: 0,
-//         shouldSwitchAutoOnNextCycle: false,
-//         transition: 'height ' + this.props.transitionTime + 'ms ' + this.props.easing
-//       });
-//     }, 50);
-//   },
+  prepareToOpen = () => {
+    //The height has been changes back to fixed pixel, we set a small timeout to force the CSS transition back to 0 on the next tick.
+    window.setTimeout(() => {
+      this.setState({
+        height: 0,
+        shouldSwitchAutoOnNextCycle: false,
+        transition: 'height ' + this.props.transitionTime + 'ms ' + this.props.easing
+      });
+    }, 50);
+  }
 
-//   render: function () {
+  render() {
 
-//     var dropdownStyle = {
-//       height: this.state.height,
-//       WebkitTransition: this.state.transition,
-//       msTransition: this.state.transition,
-//       transition: this.state.transition,
-//       overflow: this.state.overflow
-//     }
+    var dropdownStyle = {
+      height: this.state.height,
+      WebkitTransition: this.state.transition,
+      msTransition: this.state.transition,
+      transition: this.state.transition,
+      overflow: this.state.overflow
+    }
 
-//     var openClass = this.state.isClosed ? 'is-closed' : 'is-open';
+    var openClass = this.state.isClosed ? 'is-closed' : 'is-open';
 
-//     //If user wants different text when tray is open
-//     var trigger = (this.state.isClosed === false) && (this.props.triggerWhenOpen !== undefined) ? this.props.triggerWhenOpen : this.props.trigger;
+    //If user wants different text when tray is open
+    var trigger = (this.state.isClosed === false) && (this.props.triggerWhenOpen !== undefined) ? this.props.triggerWhenOpen : this.props.trigger;
 
-//     // Don't render children until the first opening of the Collapsible if lazy rendering is enabled
-//     var children = this.props.children;
-//     if(this.props.lazyRender)
-//       if(!this.state.hasBeenOpened)
-//           children = null;
+    // Don't render children until the first opening of the Collapsible if lazy rendering is enabled
+    var children = this.props.children;
+    if (this.props.lazyRender)
+      if (!this.state.hasBeenOpened)
+        children = null;
 
-//     return(
-//       <div className={this.props.classParentString}>
-//         <span className={this.props.classParentString + "__trigger" + ' ' + openClass} onClick={this.handleTriggerClick}>{trigger}</span>
-//         <div className={this.props.classParentString + "__contentOuter" } ref="outer" style={dropdownStyle}>
-//           <div className={this.props.classParentString + "__contentInner"} ref="inner">
-//             {children}
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-
-// });
+    return (
+      <div className={this.props.classParentString}>
+        <span className={this.props.classParentString + "__trigger" + ' ' + openClass} onClick={this.handleTriggerClick}>{trigger}</span>
+        <div className={this.props.classParentString + "__contentOuter"} ref="outer" style={dropdownStyle}>
+          <div className={this.props.classParentString + "__contentInner"} ref="inner">
+            {children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
