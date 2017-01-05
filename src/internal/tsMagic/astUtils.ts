c5a3eb860dcbf3785ff2257ee6e5958fcaf0b093
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-05 17:28:54 +1100

    starting parsing

--- src/internal/tsMagic/astUtils.ts
+++ src/internal/tsMagic/astUtils.ts
@@ -1,2 +1,13 @@
 import * as ts from 'typescript';
 
+let fileId = 0;
+export function createSourceFile(code: string): ts.SourceFile {
+  let sourceFile = ts.createSourceFile(
+    `${fileId++}.js`,
+    code,
+    ts.ScriptTarget.ES2015,
+    /* setParentNodes */ true,
+    ts.ScriptKind.TSX
+  );
+  return sourceFile;
+}
