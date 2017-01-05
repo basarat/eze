03ec7e202c52f1bea29baa402cba6cd903d3b2c8
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-05 15:06:11 +1100

    two level

--- src/app/components/toc.tsx
+++ src/app/components/toc.tsx
@@ -21,17 +21,27 @@ namespace TocStyles {
       }
     }
   );
+
+  export const marginLeft = (level: 1 | 2 | 3 | 4 | 5 | 6) => {
+    return {
+      marginLeft: (level * 5) + 'px'
+    };
+  }
 }
+
+const renderTocEntry = (t: types.TableOfContentEntry) => <a key={t.level + t.id} className={classes(
+  TocStyles.tocAnchorClass,
+)} style={TocStyles.marginLeft(t.level)} href={"#" + t.id}>
+  {t.text}
+</a>;
 
 export const Toc = ({ toc }: { toc: types.TableOfContentEntry[] }) => <gls.ContentVerticalContentMargined>
   <txt.H1 id={'toc'}>Table of Contents</txt.H1>
   <gls.ContentVertical>
-    {toc.map((t, index) => {
-      return <a key={index} className={classes(
-        TocStyles.tocAnchorClass,
-      )} href={"#" + t.id}>
-        {t.text}
-      </a>
-    })}
+    {
+      toc.map((t) => {
+        return [renderTocEntry(t)].concat(t.subItems.map(renderTocEntry))
+      })
+    }
   </gls.ContentVertical>
 </gls.ContentVerticalContentMargined>;
