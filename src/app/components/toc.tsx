390eef122144a8ce792d88460591354e6ff0d739
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-05 15:12:56 +1100

    first of of TOC is okay. Will add sidebar later :rose:

--- src/app/components/toc.tsx
+++ src/app/components/toc.tsx
@@ -10,7 +10,6 @@ namespace TocStyles {
   export const tocAnchorClass = style(
     csstips.padding(5, 5, 5, 10),
     {
-      background: '#eee',
       color: colors.text,
       textDecoration: 'none',
       $nest: {
@@ -24,23 +23,23 @@ namespace TocStyles {
 
   export const marginLeft = (level: 1 | 2 | 3 | 4 | 5 | 6) => {
     return {
-      marginLeft: (level * 5) + 'px'
+      marginLeft: (level * 10) + 'px'
     };
   }
 }
 
-const renderTocEntry = (t: types.TableOfContentEntry) => <a key={t.level + t.id} className={classes(
+const renderTocEntry = (t: types.TableOfContentEntry) => [<a key={t.level + t.id} className={classes(
   TocStyles.tocAnchorClass,
 )} style={TocStyles.marginLeft(t.level)} href={"#" + t.id}>
   {t.text}
-</a>;
+</a>].concat(t.subItems.map(renderTocEntry));
 
 export const Toc = ({ toc }: { toc: types.TableOfContentEntry[] }) => <gls.ContentVerticalContentMargined>
   <txt.H1 id={'toc'}>Table of Contents</txt.H1>
   <gls.ContentVertical>
     {
       toc.map((t) => {
-        return [renderTocEntry(t)].concat(t.subItems.map(renderTocEntry))
+        return renderTocEntry(t)
       })
     }
   </gls.ContentVertical>
