b855da7355ae61bbc595250c50cde206670ba806
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-01-05 17:12:51 +1100

    more

--- src/docs/story-1.tsx
+++ src/docs/story-1.tsx
@@ -1,10 +1,12 @@
 import { story } from '../story';
 import * as React from 'react';
+import { Anchor } from '../app/components/anchor';
 
 story()
   .md(`
     # Stories
-    Just as easy to show as complete application demos
+    Just as easy to show as complete application demos.
+    e.g. Lets showcase our anchor:
   `)
-  .demo(<button>Hello world</button>)
+  .demo(<Anchor href="http://www.google.com" target="_blank">As an example click here to open google.</Anchor>)
   .render();
