ebadabe4bf7c1a79dc30c4b68a44eab22f7916dd
basarat <basaratali@gmail.com>
2018-03-12 08:58:43 +1100

    cleanup old code

--- src/starts.ts
+++ src/starts.ts
@@ -6,7 +6,7 @@ starts({
     port: 4000
   },
   run: [
-    { cmd: 'npm run builddocs', watch: ['src/**', '!src/**/child.js'] },
-    { cmd: 'npm run buildtsc -- -w', watch: ['src/**/*.ts', '!src/**/child.js'] }
+    { cmd: 'npm run builddocs', watch: ['src/**'] },
+    { cmd: 'npm run buildtsc -- -w', watch: ['src/**/*.ts'] }
   ]
 })
