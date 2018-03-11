c3e5f9e19f0d87ca59d729a258e2ead61d58731d
basarat <basaratali@gmail.com>
2018-03-12 10:30:00 +1100

    sever placeholder

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,3 +2,18 @@
  * - Runs TypeScript on the fly (using ts-node)
  * - Serves a folder for live demo development
  */
+
+const serveIndex = process.argv.indexOf('--serve');
+
+// Serve mode 
+if (serveIndex !== -1) {
+  const serveFolder = process.argv[serveIndex + 1];
+
+}
+// Non serve mode
+else {
+
+}
+
+// DJ - spin that s***
+require('ts-node');
