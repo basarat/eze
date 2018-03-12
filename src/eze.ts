c30801147f48b172f49d6da0de8b80fac306f4a3
basarat <basaratali@gmail.com>
2018-03-12 18:58:27 +1100

    onchange

--- src/eze.ts
+++ src/eze.ts
@@ -1,15 +1,10 @@
-
 if (process.argv.indexOf('--serve') !== -1) {
   const nonServeArgs = process.argv.filter(x => x !== '--serve');
   /** Assume the last argument is the file you want to execute */
   const rootToExec = nonServeArgs[nonServeArgs.length - 1];
-  require('nodemon')({
-    script: rootToExec,
-    ext: 'ts',
-  });
-  process.on('SIGINT', () => {
-    process.exit(0);
-  });
+
+  const onchange = require('onchange');
+  onchange([rootToExec], process.execPath, [require.resolve('ts-node/dist/bin.js')].concat(nonServeArgs));
 } else {
   // DJ - spin that s***
   require('ts-node/dist/bin.js');
