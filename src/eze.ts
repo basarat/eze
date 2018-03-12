ae97b6dbdaa486b1daecf30f219defa489c507b0
basarat <basaratali@gmail.com>
2018-03-12 18:42:32 +1100

    nodemon running

--- src/eze.ts
+++ src/eze.ts
@@ -1,8 +1,15 @@
 
 if (process.argv.indexOf('--serve') !== -1) {
   const nonServeArgs = process.argv.filter(x => x !== '--serve');
+  /** Assume the last argument is the file you want to execute */
   const rootToExec = nonServeArgs[nonServeArgs.length - 1];
-  require('nodemon')(`-e "ts" -x "ts-node" ${rootToExec}`);
+  require('nodemon')({
+    script: rootToExec,
+    ext: 'ts',
+  });
+  process.on('SIGINT', () => {
+    process.exit(0);
+  });
 } else {
   // DJ - spin that s***
   require('ts-node/dist/bin.js');
